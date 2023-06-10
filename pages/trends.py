5d6c0d60c6b10475b9b9a7737a9dfa71efec4560
Abirame-ship-it <73090647+Abirame-ship-it@users.noreply.github.com>
2023-06-10 21:14:03 +0530

    Update trends.py

--- pages/trends.py
+++ pages/trends.py
@@ -3,12 +3,13 @@ from pytrends.request import TrendReq
 import pandas as pd
 from llama_index import download_loader
 from pandasai.llm.openai import OpenAI
+import datetime
 
 # Initialize pytrends
 pytrends = TrendReq(hl='en-US', tz=360)
 
 # List of initial keywords
-initial_keywords = ['Galle Tourism', 'Galle', 'Hotels Galle', 'Resorts Galle Srilanka','Srilanka', 'Tourist', 'locations']
+initial_keywords = ['Galle Tourism', 'Galle', 'Hotels Galle', 'Resorts Galle Srilanka', 'Srilanka', 'Tourist', 'locations']
 
 # Create a for keyword selection
 selected_keywords = st.multiselect('Select existing keywords', initial_keywords)
@@ -25,16 +26,22 @@ loader = PandasAIReader(llm=llm)
 
 # When keywords are selected, fetch data from Google Trends and display it
 if st.button('Fetch Google Trends data for selected keywords'):
-    # Define the payload
-    kw_list = selected_keywords
+    # Get the current year
+    current_year = datetime.datetime.now().year
+
+    # Calculate the previous year
+    previous_year = current_year - 1
+
+    # Define the timeframe
+    timeframe = f'{previous_year}-01-01 {previous_year}-12-31'
 
     # Get Google Trends data
-    pytrends.build_payload(kw_list, timeframe='2022-01-01 2022-12-31')
+    pytrends.build_payload(selected_keywords, timeframe=timeframe)
 
     # Get interest over time
     data = pytrends.interest_over_time()
     if not data.empty:
-        data = data.drop(labels=['isPartial'],axis='columns')
+        data = data.drop(labels=['isPartial'], axis='columns')
 
         # Save the data to the session state
         st.session_state.data = data
@@ -48,9 +55,26 @@ else:
 
 # Assuming you want to use the AI to answer questions based on the fetched data
 query = st.text_input("Enter your question")
-ask = st.button("ask")
-
+ask = st.button("Ask")
 
 if ask:
-    response = loader.run_pandas_ai(st.session_state.data, query, is_conversational_answer=True)
-    st.write(response)
+    # Check if data is available
+    if 'data' in st.session_state:
+        data = st.session_state.data
+
+        # Check if data is not empty
+        if not data.empty:
+            # Fetch the most recent data based on the date range
+            timeframe_start = data.index.min().strftime('%Y-%m-%d')
+            timeframe_end = data.index.max().strftime('%Y-%m-%d')
+            timeframe = f'{timeframe_start} {timeframe_end}'
+            pytrends.build_payload(selected_keywords, timeframe=timeframe)
+            updated_data = pytrends.interest_over_time()
+            if not updated_data.empty:
+                updated_data = updated_data.drop(labels=['isPartial'], axis='columns')
+                data = pd.concat([data, updated_data])
+
+        response = loader.run_pandas_ai(data, query, is_conversational_answer=True)
+        st.write(response)
+    else:
+        st.write("Please fetch Google Trends data first.")
